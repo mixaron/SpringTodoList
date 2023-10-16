28e4762e5a209d8678f42fe54907a7ac1088c9c1
mixaron <tosha.ryabchuk@gmail.com>
2023-10-16 16:18:23 +0400

    for push

---
+++ src/test/java/ru/mixaron/springtodolist/SpringTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mixaron.springtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
